a91d20d8a6d2ad541cb3994e81b5f9af2e2a32f1
Jessica Chen <jessica.chen@gm.com>
2018-07-31 11:55:18 -0500

    add offline notification popup modal

--- js/apps.js
+++ js/apps.js
@@ -5,6 +5,32 @@ import DBHelper from './dbhelper.js';
 
 window.updateRestaurants = updateRestaurants;
 
+/* offline notification handler */
+window.addEventListener('load', () => {
+    function handleNetworkChange(event) {
+        let modal = document.getElementById('notification');
+
+        if (navigator.onLine) {
+            modal.style.display = 'none';
+        } else {
+            modal.style.display = 'block';
+        }
+    }
+
+    window.addEventListener('online', handleNetworkChange);
+    window.addEventListener('offline', handleNetworkChange);
+});
+
+// When the user clicks anywhere outside of the modal, close it
+
+document.getElementById('notification').onclick = function(event) {
+    console.log(event);
+    console.log(event.srcElement);
+    console.log(event.target.tagName);
+    if (event.target.tagName == 'A') {
+        document.getElementById('notification').style.display = "none";
+    }
+}
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
